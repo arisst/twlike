ab14bf85d2de90295f507ba14176784362d50b0e
Aris Setyono <ariez.id@gmail.com>
2016-05-18 16:37:17 +0700

    login

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,30 @@
 const React = require('react');
 const Router = require('react-router');
+const Config = require('../Config');
 
 const Login = React.createClass({
+    getInitialState() {
+        return {
+            username: '',
+            password: ''
+        };
+    },
+
+    onChangeUsername: function(e) {
+       this.setState({username: e.target.value});
+    },
+
+    onChangePassword: function(e) {
+       this.setState({password: e.target.value});
+    },
+
     handleSubmit(e){
-        Router.browserHistory.push('/');
+        // console.log(this.state.username);
+        // console.log(this.state.password);
+        $.post(Config.API_URL+'auth', {username:this.state.username, password:this.state.password}, function (data, status) {
+            console.log(data);
+        })
+        // Router.browserHistory.push('/');
         e.preventDefault();
     },
   render() {
@@ -18,11 +39,11 @@ const Login = React.createClass({
             <p className="login-box-msg">Sign in to start your session</p>
             <form onSubmit={this.handleSubmit}>
               <div className="form-group has-feedback">
-                <input type="email" className="form-control" placeholder="Email" required="required" />
+                <input type="text" onChange={this.onChangeUsername} value={this.state.username} className="form-control" placeholder="Email" required="required" />
                 <span className="glyphicon glyphicon-envelope form-control-feedback" />
               </div>
               <div className="form-group has-feedback">
-                <input type="password" className="form-control" placeholder="Password" required="required" />
+                <input type="password" onChange={this.onChangePassword} value={this.state.password} className="form-control" placeholder="Password" required="required" />
                 <span className="glyphicon glyphicon-lock form-control-feedback" />
               </div>
               <div className="row">
